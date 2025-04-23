a860d64121cdd1e1c29af1e0e56ff02e21d99fc7
Duc Thanh Nguyen <dnguyen@kolb-digital.de>
2025-04-23 15:05:08 +0200

    webconfig auth-service change for lokal entwicklung

--- auth-service/src/main/java/com/mybookstore/authservice/config/WebConfig.java
+++ auth-service/src/main/java/com/mybookstore/authservice/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // Änderung hier: Verwende allowedOriginPatterns mit "*"
+                //.allowedOriginPatterns("*") // Änderung hier: Verwende allowedOriginPatterns mit "*"
+                .allowedOrigins(frontendUrl) // Verwende die spezifische URL statt Wildcard (testen mit http://localhost:4200 lokale Entwicklung)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
